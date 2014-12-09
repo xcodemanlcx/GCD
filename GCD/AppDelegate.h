869517258830e2cc2ab3145166398dec917ae455
hx_leichunxiang <apple@leichunxiang.local>
2014-12-09 14:22:54 +0800

    Initial Commit

---
+++ GCD/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GCD
+//
+//  Created by hx_leichunxiang on 14-12-9.
+//  Copyright (c) 2014年 lcx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
